6426f0f9ebd12b390d866fc0c4a19e72afc051fe
fuston05 <fuston05@yahoo.com>
2019-12-06 09:47:50 -0500

    got less compiler running, set up vars to work with.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// /////////////// grab elements ////////////////////
+////////////////////////////////////////////////////
+
+//images
+const introImg= document.querySelector('.intro img');
+const contentSectionImg= document.querySelector('.content-section img');
+const inverseImg= document.querySelector('.inverse-content img');
+const destinationImg= document.querySelector('.content-destination img');
+
+//buttons
+const button1= document.querySelector('.content-pick .btn:first-child');
+const button2= document.querySelector('.content-pick .btn:nth-child(2)');
+const button3= document.querySelector('.content-pick .btn:last-child');
+
+
+//nav links
+const navLink1= document.querySelector('nav .nav-link:first-child');
+const navLink2= document.querySelector('nav .nav-link:nth-child(2)');
+const navLink3= document.querySelector('nav .nav-link:nth-child(3)');
+const navLink4= document.querySelector('nav .nav-link:last-child');
+
+// main heading
+const mainHeading= document.querySelector('header h1');
+
+// /////////////// events ////////////////////
+/////////////////////////////////////////////
+
