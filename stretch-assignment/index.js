d8e57e9764479351bbccb339c46bd19ab361cf47
fuston05 <fuston05@yahoo.com>
2019-12-06 23:15:08 -0500

    still testing further to get the stretch project.. getting closer

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -6,6 +6,9 @@ const green= document.querySelector('.block--green');
 const pink= document.querySelector('.block--pink');
 const gray= document.querySelector('.block--gray');
 
+//get a nodeList of blocks for looping
+const blocks= document.querySelectorAll('.block');
+
 const blockHeight= 120; // 100px + 10px margin top and bottom
 
 //get top pos rel to topleft viewport
@@ -14,49 +17,83 @@ function getMyPos(block){
     return cur.top;
 }//end func
 
+function moveToTop(block){
+    let pos= getMyPos(block);
+    // console.log(`${block}, pos: `+ pos);
+
+    let move= Math.floor(pos/120);
+    if(move > 0){
+        // console.log('move: '+move); 
+    }//end if
+
+    //move clicked block UP
+    if( pos > 69 ){
+        gsap.to( block, { duration: 1, y: (-120 * move) } );
+    }//end if
+
+    for( let i= 0; i < blocks.length; i++){
+        let curPos= getMyPos(blocks[i]);
+        console.log(blocks[i],': curPos' + curPos);
+    
+        let curMove= Math.ceil(curPos/120);
+        console.log(blocks[i],' curMove'+ curMove);
+
+        if( curPos < pos && curMove > 0){
+            gsap.to( blocks[i], { duration: 1, y: (120) } );
+            // console.log(blocks[i],'move down');
+        }//end if
+    }//end for
+    pos= '';
+    move= '';
+    curMove= '';
+    curPos= '';
+}//end func
+
+// function reOrganize(block){ 
+//     // console.log('reOrganize called');
+//     //use curr pos of 'block'
+//     //anything with a less 'pos' move down one increment of 120 each
+//     for( let i= 0; i < blocks.length; i++){
+
+//         let curPos= getMyPos(blocks[i]);
+//         console.log('curPos' + curPos);
+//         let curMove= Math.floor(curPos/120);
+
+//         if( curPos < pos ){
+//             gsap.to( block, { duration: 1, y: (120 * curMove) } );
+//             console.log('move down');
+//         }//end if
+
+//     }//end for
+
+//     console.log('cur block: ',block);
+// }//end func
+
+
+
 // //////////////// add event listeners //////////////////
 /////////////////////////////////////////////////////////
 red.addEventListener('click', (event) => {
     event.stopPropagation();
-
-    let pos= getMyPos(red);
-    console.log('red pos: '+ pos);
-
-    let move= Math.floor(pos/120);
-    if(move > 0){
-        console.log('move: '+move); 
-    }//end if
-
-    if( pos > 69 ){
-        gsap.to( red, { duration: 1, y: (-120 * move) } );
-        console.log('Not on top');
-    }//end if
+    moveToTop(red);
 });//end click event
 
 blue.addEventListener('click', (event) => {
     event.stopPropagation();
-
-    let pos= getMyPos(blue);
-    console.log('blue pos: '+pos);
-    let move= Math.floor(pos/120);
-    if(move > 0){
-        console.log('move: '+move); 
-    }
-
-    if( pos > 68 ){
-        gsap.to( blue, { duration: 1, y: -120 } );
-        console.log('Not on top');
-    }//end if
+    moveToTop(blue);
 });//end click event
 
 green.addEventListener('click', (event) => {
-    
+    event.stopPropagation();
+    moveToTop(green);
 });//end click event
 
 pink.addEventListener('click', (event) => {
-    
+    event.stopPropagation();
+    moveToTop(pink);
 });//end click event
 
 gray.addEventListener('click', (event) => {
-    
+    event.stopPropagation();
+    moveToTop(gray);
 });//end click event
